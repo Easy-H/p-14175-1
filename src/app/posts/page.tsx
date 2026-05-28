bd155e38d199bc494d0aaf523f63aeb55bb9e0b8
EasyH <skysea001010@gmail.com>
2026-05-28 16:29:40 +0900

    13강까지 진행

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export default function Page() {
+
+    const [posts, setPosts] = useState<{id: number; title: string}[]>([]);
+
+    useEffect(() => {   
+        fetch("http://localhost:8080/api/v1/posts")
+            .then((res) => res.json())
+            .then((data) => {
+                setPosts(data);
+                console.log(data);
+            });
+
+    }, []);
+    
+    return (
+    <div>
+        <h1>글 목록</h1>
+        {}
+        <ul>
+            {
+                posts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))
+            }
+        </ul>
+    </div>)
+}
