5e6c7b94cdc056bce81f131b507acac8735d89c6
EasyH <skysea001010@gmail.com>
2026-05-29 15:56:29 +0900

    34강까지 진행

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,31 +1,43 @@
 "use client"
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { PostDto } from "@/type/post";
+import { apiFetch } from "@/lib/backend/client";
 
 export default function Page() {
 
-    const [posts, setPosts] = useState<{id: number; title: string}[]>([]);
+    const [posts, setPosts] = useState<PostDto[] | null>(null);
 
-    useEffect(() => {   
-        fetch("http://localhost:8080/api/v1/posts")
-            .then((res) => res.json())
+    useEffect(() => {
+        apiFetch(`/api/v1/posts`)
             .then((data) => {
                 setPosts(data);
                 console.log(data);
             });
 
     }, []);
-    
+
+    if (posts == null) {
+        return <div>로딩중</div>
+    }
+
     return (
-    <div>
-        <h1>글 목록</h1>
-        {}
-        <ul>
-            {
-                posts.map((post) => (
-                    <li key={post.id}>{post.title}</li>
-                ))
+        <div>
+            <h1>글 목록</h1>
+            {posts.length == 0 && <div>글이 없습니다.</div>}
+            {posts.length > 0 && (
+                <ul>
+                    {
+                        posts.map((post) => (
+                            <li key={post.id}><Link href={`/posts/${post.id}`}>{post.title}</Link></li>
+                        ))
+                    }
+                </ul>)
             }
-        </ul>
-    </div>)
+            <div>
+                <Link href="/posts/write">글쓰기</Link>
+            </div>
+        </div>
+    );
 }
